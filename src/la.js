1f7ce5dcfc084f522b6436fb7074fb8aa733328e
Tobias Kolditz <tbs.kldtz@gmail.com>
2022-11-06 08:35:38 +0100

    Add docstrings

--- src/la.js
+++ src/la.js
@@ -1,25 +1,62 @@
 class Vec {
+  /**
+   * 2D vector.
+   * @param {number} x 
+   * @param {number} y 
+   */
   constructor(x, y) {
     this.x = x;
     this.y = y;
   }
 
+  /**
+   * String representation of the form "x y".
+   * @returns {string}
+   */
   toString() {
     return `${this.x} ${this.y}`;
   }
 
+  /**
+   * Vector addition.
+   * @param {Vec} b 
+   * @returns {Vec}
+   */
   add(b) {
     return new Vec(this.x + b.x, this.y + b.y);
   }
 
+  /**
+   * Dot product.
+   * @param {number} b 
+   * @returns {number}
+   */
   dot(b) {
     return this.x * b.x + this.y * b.y;
   }
 
+  /**
+   * L2-norm.
+   * @returns {number}
+   */
+  length() {
+    return Math.sqrt(this.x ** 2 + this.y ** 2);
+  }
+
+  /**
+   * Scalar multiplication.
+   * @param {number} k 
+   * @returns {Vec}
+   */
   scale(k) {
     return new Vec(this.x * k, this.y * k);
   }
 
+  /**
+   * Counterclockwise rotation.
+   * @param {number} angle 
+   * @returns {Vec}
+   */
   rotate(angle) {
     return new Vec(
       this.x * Math.cos(angle) + this.y * Math.sin(angle),
@@ -28,6 +65,12 @@ class Vec {
   }
 }
 
+/**
+ * One-letter factory method for Vec.
+ * @param {number} x 
+ * @param {number} y 
+ * @returns {Vec}
+ */
 export function v(x, y) {
   return new Vec(x, y);
 }
